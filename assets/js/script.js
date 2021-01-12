39c186baf1d89969a8c5c5594479d29295d2d5d1
beatrizodorcik <beaodorcik@gmail.com>
2021-01-12 16:19:39 -0300

    JavaScript ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,65 @@
 let nome = window.document.getElementById('nome')
 let email = document.querySelector('#email')
 let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector('#mapa')
 
 nome.style.width = '100%'
 email.style.width = '100%'
 
 function validaNome() {
   let txtNome = document.querySelector('#txtNome')
-  if(nome.value.length < 3){
+  if (nome.value.length < 3) {
     txtNome.innerHTML = 'Nome Inválido'
     txtNome.style.color = 'red'
-  } else{
+  } else {
     txtNome.innerHTML = 'Nome Válido'
     txtNome.style.color = 'green'
+    nomeOk = true
   }
 }
 
-function validaEmail(){
+function validaEmail() {
   let txtEmail = document.querySelector('txtEmail')
-  if(email.value.indexOf('@')== -1 || email.indexOf('.')){
+  if (email.value.indexOf('@') == -1 || email.indexOf('.')) {
     txtEmail.innerHTML = "E-mail inválido"
     txtEmail.style.color = 'red'
   } else {
     txtEmail.innerHTML = "E-mail válido"
-    txtEmail.style.color = 'green'   
+    txtEmail.style.color = 'green'
+    emailOk = true
   }
+}
+
+function validaAssunto() {
+  let txtAssunto = document.querySelector('#txtAssunto')
+
+  if (assunto.value.length >= 100) {
+    txtAssunto.innerHTML = 'Excedeu o limite de caracteres, digite no máximo 100 caracteres!'
+    txtAssunt.style.color = 'red'
+    txtAssunto.style.display = 'block'
+  } else {
+    txtAssunto.style.display = 'none'
+    assuntoOk = true
+  }
+}
+
+function enviar() {
+  if(nomeOk == true && emailOk == true && assuntoOk == true){
+    alert('Formulário enviado com sucesso!')
+  }else{
+    alert('Preencha o formulário corretamente antes de enviar!')
+  }
+}
+
+function mapaZoom() {
+  mapa.style.width = '800px'
+  mapa.style.height = '600px'
+}
+
+function mapaNormal() {
+  mapa.style.width = '400px'
+  mapa.style.height = '250px'
 }
