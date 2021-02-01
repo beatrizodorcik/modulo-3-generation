556daa0e2c49062643627f1c0dc45cbe5de4221f
beatrizodorcik <beaodorcik@gmail.com>
2021-01-31 22:03:25 -0300

    model

--- BlogWithSecurityBasic-master/blogPessoal-api-master/src/main/java/generation/org/blogPessoal/model/UsuarioLogin.java
+++ BlogWithSecurityBasic-master/blogPessoal-api-master/src/main/java/generation/org/blogPessoal/model/UsuarioLogin.java
@@ -2,6 +2,8 @@ package generation.org.blogPessoal.model;
 
 public class UsuarioLogin {
 	
+	private long id;
+	
 	private String nome;
 	
 	private String usuario;
@@ -10,11 +12,21 @@ public class UsuarioLogin {
 	
 	private String token;
 	
-	private boolean vendedor;
+	private String foto;
+	
+	private String tipo;
 
 	
 	public String getNome() {
 		return nome;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
 	}
 
 	public void setNome(String nome) {
@@ -33,13 +45,6 @@ public class UsuarioLogin {
 		this.token = token;
 	}
 
-	public boolean isVendedor() {
-		return vendedor;
-	}	
-
-	public void setVendedor(boolean vendedor) {
-		this.vendedor = vendedor;
-	}
 
 	public void setUsuario(String usuario) {
 		this.usuario = usuario;
@@ -52,4 +57,21 @@ public class UsuarioLogin {
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
 }
